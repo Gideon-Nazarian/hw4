c385ad19f3a34f5afa4edabe3899ec23afe36274
Gideon Nazarian <gideonna@usc.edu>
2022-11-13 22:38:20 +0000

    problem 2

--- equal-paths.cpp
+++ equal-paths.cpp
@@ -4,10 +4,54 @@ using namespace std;
 
 // You may add any prototypes of helper functions here
 
+int treeH(Node * root)
+{
+    int LST = 0;
+    int RST = 0;
+    if(root -> left != nullptr){
+        LST = treeH(root -> left);
 
+    }
+    if(root -> right != nullptr){
+       RST =  treeH(root -> right);
+
+    }
+    if(root -> right == nullptr && root -> left == nullptr){
+        return 1;
+    }else{
+        if(LST > RST){
+            return (1 + LST);
+        }
+        else{
+            return (1 + RST);
+        }  
+    }
+    
+
+
+
+}
 bool equalPaths(Node * root)
 {
     // Add your code below
+
+    int valL = 0;
+    int valR = 0;
+
+    if(root -> left != nullptr){
+      valL = treeH(root -> left);
+    }
+    if(root -> right != nullptr){
+      valR = treeH(root -> right);
+    }
+
+    if(valL == valR){
+      return true;
+    }
+    return false;
+
+
+    
     
 
 }
